dce8b8b3fc7a6a6e88f2d0f3c0602d526676cc51
zhuzhuzhu <will_zhu1@163.com>
2022-10-08 22:30:41 +0800

    wode

--- hello/1.cpp
+++ hello/1.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(int argc, char const *argv[])
 { 
     cout << "hello world!nihao,shijie!" << endl;
-    cout <<"niubi"
+    cout <<"niubi" <<endl;
+	cout <<"hahahha"<<endl;
     system("pause");
     return 0;
 }
